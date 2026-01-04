d0c6ad3f0213dc2697ab15e60a50e2acdbb46476
Youssef Andrawes <YoussefRaafatNasry@gmail.com>
2026-01-04 06:21:27 +0100

    chore: add favicon and title

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
   import './layout.css';
-  import favicon from '$lib/assets/favicon.svg';
+  import favicon from '$lib/assets/favicon.ico';
   import GridPattern from '$lib/components/GridPattern.svelte';
 
   let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+  <title>mire</title>
+  <link rel="icon" href={favicon} sizes="any" />
+</svelte:head>
 
 <div class="min-h-screen text-gray-200">
-  <div class="min-h-screen relative z-1 p-4">
+  <div class="relative z-1 min-h-screen p-4">
     {@render children()}
   </div>
   <GridPattern
